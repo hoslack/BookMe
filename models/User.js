0844dc53203b49a7901e296a9aa9197441406165
hoslack <hoslackochieng@gmail.com>
2018-01-06 21:45:17 +0200

    setup mongodb and user schema

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+	googleID: String,
+	username: String,
+	avatar: String,
+});
+
+mongoose.model('users', userSchema);
