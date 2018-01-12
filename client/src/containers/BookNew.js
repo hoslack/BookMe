b2467e092b9e6f952c005057177c9faf8409559f
hoslack <hoslackochieng@gmail.com>
2018-01-12 16:05:31 +0200

    redirect after book submit

--- client/src/containers/BookNew.js
+++ client/src/containers/BookNew.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import BookForm from './BookForm';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import BookField from './BookField';
+import { connect } from 'react-redux';
+import { submitBook } from '../actions';
 
 class BookNew extends Component {
-	render() {
+	renderFields() {
 		return (
 			<div>
-				<BookForm />
+				<Field label="Book Title" type="text" name="title" component={BookField} />
+				<Field label="Descritpion" type="text" name="descritpion" component={BookField} />
+			</div>
+		);
+	}
+
+	onSubmit(values) {
+		this.props.submitBook(values, () => {
+			this.props.history.push('/books');
+		});
+	}
+
+	render() {
+		const { handleSubmit } = this.props;
+		return (
+			<div>
+				<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+					{this.renderFields()}
+					<Link to="/books" className="tealt btn red left">
+						Cancel
+						<i class="material-icons right">cancel</i>
+					</Link>
+					<button type="submit" className="tealt btn right">
+						Submit
+						<i class="material-icons right">send</i>
+					</button>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default BookNew;
+const validate = values => {
+	const errors = {};
+	if (!values.title) {
+		errors.title = 'Please enter a title';
+	}
+	if (!values.descritpion) {
+		errors.descritpion = 'Please enter a title';
+	}
+
+	return errors;
+};
+
+export default reduxForm({
+	validate,
+	form: 'bookForm',
+})(connect(null, { submitBook })(BookNew));
